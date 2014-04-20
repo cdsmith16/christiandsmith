6478f8053e35dcf2e5a4b3020febd28827eacd30
csmooth <christiandsmith1990@gmail.com>
2014-04-19 22:37:04 -0700

    Added basic animations to resume page
    Added basic animations to the resume ("books") page using CSS3 and
    Javascript

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,15 @@
-console.log('This would be the main JS file.');
+var parents = document.getElementsByClassName("parent");
+
+function bounce(index){
+	this.parents[index].classList.add('wiggle');
+}
+
+function bounce_wrapper(value) {
+    return function () {
+        bounce(value)
+    }
+}
+
+for(i=0; i<parents.length; i++){
+	setTimeout(bounce_wrapper(i),(i*4000));
+}
